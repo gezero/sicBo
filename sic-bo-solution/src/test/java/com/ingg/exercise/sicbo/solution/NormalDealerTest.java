8bc40a41f4873dd664a4617626023b655c3f064c
Jiri Peinlich <jiri.peinlich@gmail.com>
2014-07-29 14:12:40 +0200

    refactored test to be less random

--- sic-bo-solution/src/test/java/com/ingg/exercise/sicbo/solution/NormalDealerTest.java
+++ sic-bo-solution/src/test/java/com/ingg/exercise/sicbo/solution/NormalDealerTest.java
@@ -3,23 +3,28 @@ package com.ingg.exercise.sicbo.solution;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertThat;
 
 public class NormalDealerTest {
 
 
-    private static final byte[] SEED = {1,2,3,4,5};
+    private static final byte[] SEED = {1, 2, 3, 4, 5};
+    private static final long EPS = 100_000_000L; // this many nanoseconds should be 100 milliseconds
 
     @Test
     public void testFlow() throws Exception {
-        NormalDealer dealer = new NormalDealer(new SessionRandomGenerator(SEED), 300);
+        int timeout = 300;
+        NormalDealer dealer = new NormalDealer(new SessionRandomGenerator(SEED), timeout);
 
-        RollCollector rollCollector = new RollCollector();
+        int rounds = 5;
+        CountDownLatch latch = new CountDownLatch(rounds);
+
+        RollCollector rollCollector = new RollCollector(latch);
         Iterable<Integer> firstRoll = dealer.subscribe(rollCollector);
         assertThat(firstRoll, is(notNullValue()));
         int count = 0;
@@ -29,23 +34,33 @@ public class NormalDealerTest {
         assertThat(count, is(3));
         assertThat(rollCollector.getList().size(), is(0));
 
-        Thread.sleep(1000);
+        long timeStart = System.nanoTime(); //to find out why not to use System.currentTimeMillis() check this StackOverflow answer: http://stackoverflow.com/a/1776053/550859
+        latch.await();
+        long timeStop = System.nanoTime();
 
         dealer.stop();
 
-        assertThat(rollCollector.getList().size(), is(3));
+        assertThat(timeStop - timeStart, is(lessThan(timeout * rounds * 1000 * 1000 + EPS)));
+        assertThat(rollCollector.getList().size(), is(equalTo(rounds)));
 
-        Thread.sleep(300);
-        assertThat(rollCollector.getList().size(), is(3));
+        Thread.sleep(500);
+        assertThat(rollCollector.getList().size(), is(rounds));
 
     }
 
     private class RollCollector implements DealerObserver {
         public List<Iterable<Integer>> list = new ArrayList<>();
+        private CountDownLatch latch;
+
+        public RollCollector(CountDownLatch latch) {
+
+            this.latch = latch;
+        }
 
         @Override
         public void newRoll(Iterable<Integer> roll) {
             list.add(roll);
+            latch.countDown();
         }
 
         public List<Iterable<Integer>> getList() {
@@ -54,9 +69,9 @@ public class NormalDealerTest {
     }
 
     @Test(expected = RuntimeException.class)
-    public void testCannotSubscribeTwice(){
-        NormalDealer dealer = new NormalDealer(new SessionRandomGenerator(SEED),100);
-        RollCollector rollCollector = new RollCollector();
+    public void testCannotSubscribeTwice() {
+        NormalDealer dealer = new NormalDealer(new SessionRandomGenerator(SEED), 100);
+        RollCollector rollCollector = new RollCollector(null);
         dealer.subscribe(rollCollector);
         dealer.subscribe(rollCollector);
 
